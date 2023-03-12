a00deb96a89f4192302af73a3ee8b51840ef9e72
pangzixiang <313227220@qq.com>
2023-03-12 10:44:07 +0800

    [UPGRADE] websocket logic enhancement

--- src/test/java/io/github/pangzixiang/whatsit/vertx/core/local/filter/WebSocketTestFilter.java
+++ src/test/java/io/github/pangzixiang/whatsit/vertx/core/local/filter/WebSocketTestFilter.java
@@ -9,6 +9,6 @@ public class WebSocketTestFilter implements WebsocketFilter {
     @Override
     public boolean doFilter(ServerWebSocket serverWebSocket) {
         log.info("invoke");
-        throw new RuntimeException("test");
+        return true;
     }
 }
